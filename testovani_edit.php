072fe402ccf89ee0e9ac10ddc826d4b861b3ed9c
Jan Bessa Urbánek <git@zirland.org>
2021-01-23 18:39:57 +0100

    Edit test

--- testovani_edit.php
+++ testovani_edit.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 <html>
 <head>
 	<meta content="text/html; charset=utf-8" http-equiv="content-type">
-	<title>Editace hlásky</title>
+	<title>Editace testu hlásek</title>
 	<script type="text/javascript" src="https://api.mapy.cz/loader.js"></script>
 	<script type="text/javascript">
 		Loader.lang = "cs";
@@ -22,7 +22,34 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 	</script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
-        body{ font: 14px sans-serif; }
+        body {
+            font: 14px sans-serif;
+        }
+
+        .wrapper {
+            width: 500px;
+            padding: 20px;
+        }
+
+        tr.dark {
+            background-color: #ddd;
+            color: black;
+        }
+
+        tr.light {
+            background-color: #fff;
+            color: black;
+        }
+
+        tr.dark-smoke {
+            background-color: #8b0000;
+            color: white;
+        }
+
+        tr.light-smoke {
+            background-color: #dc143c;
+            color: white;
+        }
     </style>
 </head>
 
@@ -169,30 +196,66 @@ mysqli_stmt_close($stmt);
 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
 <input type="hidden" name="id" value="<?php echo $id; ?>">
 <input type="hidden" name="action" value="hlasky">
-<table width="20%" style="text-align:left;">
+<table style="text-align:left;">
 <tr><td colspan="2"><input type="submit" value="Uložit seznam hlásek"></form></td></tr>
 
 <?php
-$z   = 0;
+$z            = 0;
 $hlasky_array = explode("|", $old_hlasky);
 
-$sql = "SELECT id, tel_cislo, kilometr, smer FROM hlasky WHERE silnice = '$old_silnice' ORDER BY CAST(kilometr AS unsigned), smer";
-if ($stmt = mysqli_prepare($link, $sql)) {
-    if (mysqli_stmt_execute($stmt)) {
-        mysqli_stmt_bind_result($stmt, $hl_id, $hl_telcislo, $hl_kilometr, $hl_smer);
+unset($strediska);
 
-        while (mysqli_stmt_fetch($stmt)) {
-            echo "<tr><td><input type=\"checkbox\" name=\"line$z\" value=\"$hl_id\"";
-            if (in_array($hl_id,$hlasky_array)) {
+$query179 = "SELECT DISTINCT ssud FROM hlasky WHERE silnice = '$old_silnice' ORDER BY CAST(kilometr AS unsigned), smer;";
+if ($result179 = mysqli_query($link, $query179)) {
+    while ($row179 = mysqli_fetch_row($result179)) {
+        $strediska[] = $row179[0];
+    }
+}
+$strediska = array_filter($strediska);
+
+echo "<tr>";
+foreach ($strediska as $stredisko) {
+    $ssud_nazev = "";
+    echo "<td style=\"padding:10px\"><table>";
+    $query237 = "SELECT popis FROM enum_ssud WHERE id = '$stredisko';";
+    if ($result237 = mysqli_query($link, $query237)) {
+        while ($row237 = mysqli_fetch_row($result237)) {
+            $ssud_nazev = $row237[0];
+        }
+    }
+    echo "<tr><th colspan=\"2\">$ssud_nazev</th></tr>";
+    $i        = 0;
+    $query193 = "SELECT id, tel_cislo, kilometr, smer, smoketest FROM hlasky WHERE silnice = '$old_silnice' AND ssud = '$stredisko' ORDER BY CAST(kilometr AS unsigned), smer";
+    if ($result193 = mysqli_query($link, $query193)) {
+        while ($row193 = mysqli_fetch_row($result193)) {
+            $hl_id       = $row193[0];
+            $hl_telcislo = $row193[1];
+            $hl_kilometr = $row193[2];
+            $hl_smer     = $row193[3];
+            $hl_smoke    = $row193[4];
+
+            echo "<tr class=\"";
+            if ($i % 2 == 0) {
+                echo "dark";
+            } else {
+                echo "light";
+            }
+            if ($hl_smoke == 0) {
+                echo "-smoke";
+            }
+            echo "\"><td><input type=\"checkbox\" name=\"line$z\" value=\"$hl_id\"";
+            if (in_array($hl_id, $hlasky_array)) {
                 echo " CHECKED";
             }
             echo "></td>";
             echo "<td>$hl_telcislo | km $hl_kilometr směr $hl_smer</td></tr>\n";
             $z = $z + 1;
+            $i = $i + 1;
         }
     }
+    echo "</table></td>";
 }
-mysqli_stmt_close($stmt);
+echo "</tr>";
 ?>
 <tr><td colspan="2"><input type="hidden" name="pocet" value="<?php echo $z - 1; ?>"></form></td></tr>
 </table>
